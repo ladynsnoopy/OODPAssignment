1d5bdf166fcda3f9a830a79b4c67bd38e08d18ea
ladynsnoopy <49613300+ladynsnoopy@users.noreply.github.com>
2019-10-25 16:44:01 +0800

    2nd update

--- Movie_list.java
+++ Movie_list.java
@@ -1,11 +1,16 @@
 package cinema;
 
-
+import java.util.ArrayList;
 
 public class Movie_list {
 	private Movie[] movieShowing;
 	private Movie[] movieComingSoon;
-
+	private ArrayList<Movie> movie_list;
+	
+	public Movie_list()
+	{
+		movie_list = new ArrayList<Movie>();
+	}
 	public Movie[] getMovieShowing() {
 		return this.movieShowing;
 	}
@@ -21,42 +26,52 @@ public class Movie_list {
 	public void setMovieComingSoon(Movie[] aMovieComingSoon) {
 		this.movieComingSoon = aMovieComingSoon;
 	}
-
+	public void addMovieToList(Movie movie) {
+		movie_list.add(movie);
+	}
 	/**
 	 * sort  by sales
 	 * implement comparator somewhere and compareTo
 	 */
-	public void sortSales(Comparable[] list) {
+	public void sortSales() {
 		int min;
-		Comparable temp;
-		for(int i = 0; i<list.length;i++)
+		Movie temp;
+		for(int i = 0; i<movie_list.size();i++)
 		{
 			min = i;
-			if(list[i].compareTo(list[min]) <0)
+			if(movie_list.get(i).compareTo(movie_list.get(min)) <0)
 			{
 				min = i;
 			}
-			temp = list[min];
-			list[min] = list[i];
-			list[i] = temp;
+			temp = movie_list.get(min);
+			movie_list.set(min, movie_list.get(i));
+			movie_list.set(i, temp);
 		}
 	}
-	public Movie[] sortRatings(Movie[] a)
+	public void sortRatings()
 	{
 		Movie temp;
 		// arrange according to CustomerID
-		for (int i = 1; i < a.length; i++) {
+		for (int i = 1; i < movie_list.size(); i++) {
 			for (int j = i; j > 0; j--) {
-				if (a[j].getOverall_rating() < a[j - 1].getOverall_rating()) {
-					temp = a[j - 1].copy();
-					a[j - 1] = a[j].copy();
-					a[j] = temp;
+				if (movie_list.get(j).getOverall_rating() < movie_list.get(j-1).getOverall_rating()) {
+					temp = movie_list.get(j-1).copy();
+					movie_list.set(j-1, movie_list.get(j).copy());
+					movie_list.set(j, temp);
 
 				} else
 					break;
 			}
 		}
-		return a;
+		
+	}
+	
+	public void printMovieList() {
+		System.out.println("List of ratings and reviews:");
+		for(int i = 0; i< movie_list.size();i++)
+		{
+			System.out.println(movie_list.get(i).getTitle() + " | Ratings: "+ movie_list.get(i).getOverall_rating());
+		}
 	}
 
 
