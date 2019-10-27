76ec295085e25f103dc6e9c2c7399efc381fbb3e
espressodepresso <56994283+espressodepresso@users.noreply.github.com>
2019-10-27 21:36:46 +0800

    Update readwriteTest.java

--- readwriteTest.java
+++ readwriteTest.java
@@ -5,34 +5,33 @@ import java.util.List;
 
 public class readwriteTest {
 	public static void main(String[] args) {
-		
-		//read data test
-		//returns an array of string array
-		List<String[]> csvData =  new ArrayList<String[]>();
-		csvData = csvRW.readCSV("staffdatabase");
-		//example of going thru data-change the string to int
-		//int i = Integer.parseInt(csvData.get(0)[0]);
-		//System.out.println(i);
-		//more example to just print all the data
-		for (int j=0; j<csvData.size();j++) {
-			System.out.println(csvData.get(j)[0]);
-		}
-		
-		//search example to find the row data is in
-		//i can change this to return the whole row instead
-		//search returns -1 if data not there
-		int a = csvRW.search("staffdatabase", "gv003");
-		System.out.println(a);
-		
-		
-		
+
+		// read data test
+		// returns an array of string array
+		// List<String[]> csvData = new ArrayList<String[]>();
+		// csvData = csvRW.readCSV("staffdatabase");
+		// example of going thru data-change the string to int
+		// int i = Integer.parseInt(csvData.get(0)[0]);
+		// System.out.println(i);
+		// more example to just print all the data
+//		for (int j=0; j<csvData.size();j++) {
+//			System.out.println(csvData.get(j)[0]);
+//		}
+
+		// search example to find the row data is in
+		// i can change this to return the whole row instead
+		// search returns -1 if data not there
+		// int a = csvRW.search("staffdatabase", "gv003");
+		// System.out.println(a);
+
 //  	//adding example
-//		List<String> addstuff = new ArrayList<String>();
-//		addstuff.add("gv003");
-//		addstuff.add("password123");
-//		csvRW.writeToCSV("staffdatabase", addstuff);
-//		
+		List<String> addstuff = new ArrayList<String>();
+		addstuff.add("gv009");
+		addstuff.add("password123,lol help,yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaaa");
+		csvRW.writeToCSV("staffdatabase", addstuff);
+////		
 //		//delete example
 //		csvRW.delete("staffdatabase", 6);
+		csvRW.editCSV("staffdatabase", 3, 1, 2, "yeehaw");
 	}
 }
